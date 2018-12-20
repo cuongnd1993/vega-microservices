6b7654f25ffdeb828e8602ee0431e2d161aa3847
cuongnd <cuongnd@vega.com.vn>
2018-12-20 09:41:26 +0700

    create project

---
+++ src/main/java/com/vega/microservice/VegaMicroservicesApplication.java
@@ -0,0 +1,14 @@
+package com.vega.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegaMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VegaMicroservicesApplication.class, args);
+    }
+
+}
+
---
+++ vega-microservices-zuul-api-gateway-server/src/test/java/com/vega/microservice/zuul/api/gateway/server/ZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.vega.microservice.zuul.api.gateway.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZuulApiGatewayServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
